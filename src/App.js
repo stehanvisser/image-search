8b8401faa5a57c6cb0d745f0a8ae487d27cf91e5
stehan <stehan@fastcomm.com>
2022-02-16 20:26:41 +0200

    added hello

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
   return (
     <div className="background-container">
       <SearchForm imageUrl={imageUrl} updateImageUrl={setImageUrl}></SearchForm>
+      HELLO
     </div>
   );
 };
